88e38f19724d4156959e6e210faaf5ab27f682e6
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-12-02 08:50:06 +0000

    Day 2 second half

--- 2-password-philosophy/password_validator.py
+++ 2-password-philosophy/password_validator.py
@@ -2,12 +2,13 @@ class PasswordValidator:
     def __init__(self, line):
         line = line.replace("\n", "")
         row_items = line.split(" ")
-        min_max = row_items[0].split("-")
-        self.min = int(min_max[0])
-        self.max = int(min_max[1])
+        indexes = row_items[0].split("-")
+        self.index_1 = int(indexes[0]) - 1
+        self.index_2 = int(indexes[1]) - 1
         self.required_character = row_items[1][0]
         self.password = row_items[2]
 
     def is_valid(self):
-        count = self.password.count(self.required_character)
-        return self.min <= count <= self.max
+        character_1 = self.password[self.index_1]
+        character_2 = self.password[self.index_2]
+        return (character_1 == self.required_character) != (character_2 == self.required_character)
