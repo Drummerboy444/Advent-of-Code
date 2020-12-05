69517d8ac1e1d21b5c7763ace309297815ec48fe
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-12-05 09:43:06 +0000

    Day 5 second half

--- 5-binary-boarding/main.py
+++ 5-binary-boarding/main.py
@@ -10,31 +10,34 @@ binary_digit_lookup = {
 }
 
 
-def number_from_characters(characters, base=2):
+def number_from_characters(characters):
     total = 0
     exponent = 0
 
     for character in reversed(characters):
         binary_digit = binary_digit_lookup[character]
-        total += binary_digit * (base ** exponent)
+        total += binary_digit * (2 ** exponent)
         exponent += 1
 
     return total
 
 
 def get_seat_id(boarding_pass):
-    raw_row = boarding_pass[:7]
-    raw_col = boarding_pass[7:10]
-    row = number_from_characters(raw_row)
-    col = number_from_characters(raw_col)
+    row = number_from_characters(boarding_pass[:7])
+    col = number_from_characters(boarding_pass[7:10])
     return (row * 8) + col
 
 
-highest_seat_id = 0
+seat_ids = [get_seat_id(boarding_pass) for boarding_pass in boarding_passes]
+seat_ids.sort()
 
-for boarding_pass in boarding_passes:
-    seat_id = get_seat_id(boarding_pass)
-    if seat_id > highest_seat_id:
-        highest_seat_id = seat_id
+expected_seat_id = seat_ids[0]
+missing_seat_id = None
+for seat_id in seat_ids:
+    if seat_id == expected_seat_id:
+        expected_seat_id += 1
+        continue
+    missing_seat_id = expected_seat_id
+    break
 
-print(highest_seat_id)
+print(missing_seat_id)
