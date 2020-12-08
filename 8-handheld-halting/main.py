f544e53efea804c70055c49b3ee34bfe42bb5e8e
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-12-08 15:07:26 +0000

    Day 8 both parts together

--- 8-handheld-halting/main.py
+++ 8-handheld-halting/main.py
@@ -3,11 +3,14 @@ from program import Program
 with open("input.txt") as file:
     lines = file.read().split("\n")[:-1]
 
+initial_program = Program(lines)
+initial_program.run()
+print(f"Initial program accumulator: {initial_program.accumulator}")
+
 for i in range(len(lines)):
     program = Program(lines)
     program.flip_instruction(i)
     program.run()
     if program.finished():
-        print(f"Successful program by flipping instruction {i}")
-        print(f"Accumulator {program.accumulator}")
+        print(f"Fixed program accumulator: {program.accumulator}")
         break
