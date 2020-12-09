c23fa6f9a8c6cd8b7758fe60f4eb816a923581f7
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-12-09 08:53:29 +0000

    Day 9 neatening up

--- 9-encoding-error/main.py
+++ 9-encoding-error/main.py
@@ -4,7 +4,7 @@ with open("input.txt") as file:
 
 def contains_pair_that_sum(numbers, target):
     for i in range(len(numbers) - 1):
-        for j in range(i, len(numbers)):
+        for j in range(i + 1, len(numbers)):
             if numbers[i] + numbers[j] == target:
                 return True
     return False
@@ -12,24 +12,23 @@ def contains_pair_that_sum(numbers, target):
 
 def get_first_invalid_number(numbers):
     for i in range(25, len(numbers)):
-        if not contains_pair_that_sum(numbers[i-25:i], numbers[i]):
+        if not contains_pair_that_sum(numbers[i - 25:i], numbers[i]):
             return numbers[i]
 
 
-print(f"First invalid number is {get_first_invalid_number(lines)}")
+first_invalid_number = get_first_invalid_number(lines)
+print(f"First invalid number: {first_invalid_number}")
 
 
 def get_list_that_sums_to(numbers, target):
     for i in range(len(numbers)):
-        potential_list = numbers[i:i+2]
-        to_append = i + 2
+        potential_list = numbers[i:i + 2]
         while sum(potential_list) < target:
-            potential_list.append(numbers[to_append])
-            to_append += 1
+            potential_list.append(numbers[i + len(potential_list)])
         if sum(potential_list) == target:
             return potential_list
 
 
-list_that_sums_to = get_list_that_sums_to(lines, get_first_invalid_number(lines))
+list_that_sums_to = get_list_that_sums_to(lines, first_invalid_number)
 list_that_sums_to.sort()
-print(f"Encryption weakness is {list_that_sums_to[0] + list_that_sums_to[-1]}")
+print(f"Encryption weakness: {list_that_sums_to[0] + list_that_sums_to[-1]}")
