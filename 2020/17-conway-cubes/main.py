c876bc85326e0f08961e04abfd76a4932de86ab9
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-12-17 14:28:41 +0000

    Day 17 part 2

--- 2020/17-conway-cubes/main.py
+++ 2020/17-conway-cubes/main.py
@@ -8,24 +8,26 @@ class Cube(NamedTuple):
     x: int
     y: int
     z: int
+    w: int
 
 
 cubes = []
 for i in range(len(lines)):
     for j in range(len(lines[i])):
         if lines[i][j] == "#":
-            cubes.append(Cube(j, i, 0))
+            cubes.append(Cube(j, i, 0, 0))
 
 
-def count_neighbours(cubes, x, y, z):
+def count_neighbours(cubes, x, y, z, w):
     count = 0
     for test_x in range(x - 1, x + 2):
         for test_y in range(y - 1, y + 2):
             for test_z in range(z - 1, z + 2):
-                if test_x == x and test_y == y and test_z == z:
-                    continue
-                if Cube(test_x, test_y, test_z) in cubes:
-                    count += 1
+                for test_w in range(w - 1, w + 2):
+                    if test_x == x and test_y == y and test_z == z and test_w == w:
+                        continue
+                    if Cube(test_x, test_y, test_z, test_w) in cubes:
+                        count += 1
     return count
 
 
@@ -36,24 +38,25 @@ def get_next_generation(cubes):
     max_y = max(cube.y for cube in cubes)
     min_z = min(cube.z for cube in cubes)
     max_z = max(cube.z for cube in cubes)
+    min_w = min(cube.w for cube in cubes)
+    max_w = max(cube.w for cube in cubes)
 
     next_generation = []
 
     for x in range(min_x - 1, max_x + 2):
         for y in range(min_y - 1, max_y + 2):
             for z in range(min_z - 1, max_z + 2):
-                neighbour_count = count_neighbours(cubes, x, y, z)
-                if Cube(x, y, z) in cubes and 2 <= neighbour_count <= 3:
-                    next_generation.append(Cube(x, y, z))
-                elif neighbour_count == 3:
-                    next_generation.append(Cube(x, y, z))
+                for w in range(min_w - 1, max_w + 2):
+                    neighbour_count = count_neighbours(cubes, x, y, z, w)
+                    if Cube(x, y, z, w) in cubes and 2 <= neighbour_count <= 3:
+                        next_generation.append(Cube(x, y, z, w))
+                    elif neighbour_count == 3:
+                        next_generation.append(Cube(x, y, z, w))
 
     return next_generation
 
 
-for _ in range(6):
+for i in range(6):
+    print(f"Calculating generation {i}")
     cubes = get_next_generation(cubes)
-print(f"Part 1: {len(cubes)}")
-
-
-
+print(f"Part 2: {len(cubes)}")
