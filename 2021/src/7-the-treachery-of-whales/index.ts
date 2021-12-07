0c2e08d41a931b67af8854106319ce4094ac68ec
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-12-07 19:27:21 +0000

    2021 Day 7 part 2

--- 2021/src/7-the-treachery-of-whales/index.ts
+++ 2021/src/7-the-treachery-of-whales/index.ts
@@ -1,4 +1,4 @@
-import {flow, pipe} from "fp-ts/function"
+import {pipe} from "fp-ts/function"
 import * as RONEA from "fp-ts/ReadonlyNonEmptyArray"
 import * as S from "fp-ts/string"
 import {readFile} from "../utils/file-reading"
@@ -9,26 +9,43 @@ const positions = pipe(
   RONEA.map(Number),
 )
 
-const getFuelCost: (to: number) => (from: number) => number = (to) => (from) =>
-  Math.abs(from - to)
-
 const getFuelCosts: (
-  to: number,
-) => (froms: RONEA.ReadonlyNonEmptyArray<number>) => number = (to) =>
-  flow(
-    RONEA.reduce(0, (previous, current) => previous + getFuelCost(to)(current)),
-  )
+  fuelCostCalculator: (to: number) => (from: number) => number,
+) => (to: number) => (froms: RONEA.ReadonlyNonEmptyArray<number>) => number =
+  (fuelCostCalculator) => (to) =>
+    RONEA.reduce(
+      0,
+      (previous, current) => previous + fuelCostCalculator(to)(current),
+    )
 
-const getMinFuelCost = (positions: RONEA.ReadonlyNonEmptyArray<number>) => {
+const getMinFuelCost = (
+  positions: RONEA.ReadonlyNonEmptyArray<number>,
+  fuelCostCalculator: (to: number) => (from: number) => number,
+) => {
   const minPosition = Math.min(...positions)
   const maxPosition = Math.max(...positions)
 
   let minFuelCost = Infinity
   for (let i = minPosition; i <= maxPosition; i++) {
-    minFuelCost = Math.min(minFuelCost, getFuelCosts(i)(positions))
+    minFuelCost = Math.min(
+      minFuelCost,
+      getFuelCosts(fuelCostCalculator)(i)(positions),
+    )
   }
 
   return minFuelCost
 }
 
-console.log(`Part 1: ${getMinFuelCost(positions)}`)
+const getLinearFuelCost: (to: number) => (from: number) => number =
+  (to) => (from) =>
+    Math.abs(from - to)
+
+console.log(`Part 1: ${getMinFuelCost(positions, getLinearFuelCost)}`)
+
+const getTriangleFuelCost: (to: number) => (from: number) => number =
+  (to) => (from) => {
+    const n = Math.abs(from - to)
+    return (n * (n + 1)) / 2
+  }
+
+console.log(`Part 2: ${getMinFuelCost(positions, getTriangleFuelCost)}`)
