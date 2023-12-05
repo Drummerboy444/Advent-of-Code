8bfddf1b4a70c5b61bfa5d380c9f16c3559ed038
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2023-12-05 18:08:34 +0000

    2023 Day 5 part 2

--- 2023/src/05/index.ts
+++ 2023/src/05/index.ts
@@ -76,6 +76,45 @@ const solvePart1 = (input: ParsedInput) => {
   return Math.min(...locationNumbers);
 };
 
+const solvePart2 = (input: ParsedInput) => {
+  const seedPairs: [number, number][] = [];
+
+  for (let i = 0; i < input.seeds.length / 2; i += 2) {
+    const start = input.seeds[i];
+    const length = input.seeds[i + 1];
+    if (start === undefined || length === undefined) throw new Error();
+    seedPairs.push([start, length]);
+  }
+
+  let minLocationNumber = Infinity;
+  let computationCount = 0;
+  const totalRequiredComputationCount = seedPairs
+    .map(([_, length]) => length)
+    .reduce((a, b) => a + b, 0);
+
+  seedPairs.forEach(([start, length]) => {
+    let x = start;
+    while (x < start + length) {
+      if (computationCount % 1_000_000 === 0)
+        console.log(
+          `Computing: ${Math.round(
+            (computationCount / totalRequiredComputationCount) * 100
+          )}% complete`
+        );
+
+      const loc = getLocationNumber(x, input.almanacMaps);
+      if (loc < minLocationNumber) minLocationNumber = loc;
+
+      computationCount++;
+      x++;
+    }
+  });
+
+  return minLocationNumber;
+};
+
 const file = readFile("src/05/inputs/input.txt");
 const parsedInput = parseFile(file);
+
 console.log("Part 1:", solvePart1(parsedInput));
+console.log("Part 2:", solvePart2(parsedInput));
