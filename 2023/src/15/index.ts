0fea067f35bd548a75a4075cc1a7d9b80fb79333
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2023-12-15 11:00:25 +0000

    2023 Day 15 part 2

--- 2023/src/15/index.ts
+++ 2023/src/15/index.ts
@@ -9,8 +9,72 @@ const hash = (string: string) =>
       0
     );
 
-const file = readFile("src/15/inputs/input.txt");
-const initialisationSequence = file.split(",");
+type InitialisationSequence = (
+  | {
+      type: "-";
+      label: string;
+    }
+  | {
+      type: "=";
+      label: string;
+      focalLength: number;
+    }
+)[];
 
-const part1 = initialisationSequence.map(hash).reduce((a, b) => a + b, 0);
+const toInitialisationSequence = (strings: string[]): InitialisationSequence =>
+  strings.map((string) => {
+    const lastCharacter = string[string.length - 1];
+    if (lastCharacter === "-")
+      return { type: "-" as const, label: string.slice(0, string.length - 1) };
+
+    const [label, stringFocalLength] = string.split("=");
+    if (label === undefined || stringFocalLength === undefined)
+      throw new Error();
+
+    return {
+      type: "=" as const,
+      label,
+      focalLength: parseInt(stringFocalLength),
+    };
+  });
+
+const solvePart2 = (initialisationSequence: InitialisationSequence) => {
+  const boxes: {
+    label: string;
+    focalLength: number;
+  }[][] = Array.from({ length: 256 }).map(() => []);
+
+  initialisationSequence.forEach((item) => {
+    const boxIndex = hash(item.label);
+    const box = boxes[boxIndex];
+    if (box === undefined) throw new Error();
+
+    const lensIndex = box.findIndex(({ label }) => label === item.label);
+
+    if (item.type === "-") {
+      if (lensIndex !== -1) box.splice(lensIndex, 1);
+    } else {
+      const lens = { label: item.label, focalLength: item.focalLength };
+      if (lensIndex !== -1) {
+        box[lensIndex] = lens;
+      } else {
+        box.push(lens);
+      }
+    }
+  });
+
+  return boxes
+    .flatMap((box, i) =>
+      box.map((item, j) => (i + 1) * (j + 1) * item.focalLength)
+    )
+    .reduce((a, b) => a + b, 0);
+};
+
+const file = readFile("src/15/inputs/input.txt");
+const strings = file.split(",");
+
+const part1 = strings.map(hash).reduce((a, b) => a + b, 0);
 console.log("Part 1:", part1);
+
+const part2 = solvePart2(toInitialisationSequence(strings));
+console.log("Part 2:", part2);
